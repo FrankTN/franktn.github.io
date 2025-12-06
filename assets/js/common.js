65421ed6bab562c268355d922c8102dd257544f7
Frank te Nijenhuis <f.tenijenhuis@erasmusmc.nl>
2025-12-06 10:15:03 +0100

    fixed issue with toc plugin

--- assets/js/common.js
+++ assets/js/common.js
@@ -31,6 +31,74 @@ $(document).ready(function () {
     });
   }
 
+  // Generate table of contents for beginning TOC
+  if ($("#table-of-contents").length) {
+    var tocContainer = $("#table-of-contents");
+    var markdownContent = $("#markdown-content");
+    
+    if (markdownContent.length) {
+      var headings = markdownContent.find("h1, h2, h3, h4, h5, h6");
+      var tocList = $("<ul></ul>");
+      var stack = [{ list: tocList, level: 0 }];
+      
+      headings.each(function(index) {
+        var heading = $(this);
+        var level = parseInt(heading.prop("tagName").substring(1));
+        var text = heading.text();
+        
+        // Generate ID if not present
+        var id = heading.attr("id");
+        if (!id) {
+          id = text.toLowerCase()
+            .replace(/[^\w\s-]/g, "")
+            .replace(/\s+/g, "-")
+            .replace(/-+/g, "-")
+            .replace(/^-|-$/g, "");
+          
+          // Ensure unique ID
+          var originalId = id;
+          var counter = 1;
+          while ($("#" + id).length > 0) {
+            id = originalId + "-" + counter;
+            counter++;
+          }
+          
+          heading.attr("id", id);
+        }
+        
+        // Find the appropriate parent list in the stack
+        while (stack.length > 1 && stack[stack.length - 1].level >= level) {
+          stack.pop();
+        }
+        
+        var parentStack = stack[stack.length - 1];
+        var parentList = parentStack.list;
+        
+        // Check if next heading is deeper (to decide if we need nested list)
+        var nextHeading = headings.eq(index + 1);
+        var nextLevel = nextHeading.length ? parseInt(nextHeading.prop("tagName").substring(1)) : 0;
+        var needsNested = nextLevel > level;
+        
+        // Create list item
+        var listItem = $("<li></li>");
+        var link = $("<a></a>").attr("href", "#" + id).text(text);
+        listItem.append(link);
+        parentList.append(listItem);
+        
+        // If next heading is deeper, create nested list and push to stack
+        if (needsNested) {
+          var nestedList = $("<ul></ul>");
+          listItem.append(nestedList);
+          stack.push({ list: nestedList, level: level });
+        }
+      });
+      
+      if (tocList.children().length > 0) {
+        tocContainer.append(tocList);
+      }
+    }
+  }
+
   // add css to jupyter notebooks
   const cssLink = document.createElement("link");
   cssLink.href = "../css/jupyter.css";
